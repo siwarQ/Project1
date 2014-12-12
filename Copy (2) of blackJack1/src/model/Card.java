9a8c023322977ce6252a4da3d3565cbc0fa039c5
siwarQ <socoro92@hotmail.com>
2014-12-12 11:31:39 +0200

    last update

---
+++ Copy (2) of blackJack1/src/model/Card.java
@@ -0,0 +1,127 @@
+package model;
+
+
+
+/**change hellloooooooooooooo */
+public class Card {
+	
+	//***************************************** Variables ******************************************
+	/**KEY field, Card's valu*/
+	private int value;
+	/**color of card*/
+	private Color color;
+	/**suit of card*/
+	private Suit suit;
+	/**letter of card*/
+	private Letter letter;
+	/**source of image to card*/
+	private String image;
+			
+	//***************************************** Constructors ******************************************
+	/**
+	 * Full C'tor 
+	 */
+	public Card (int value, Color color, Suit suit, String image)
+	{
+		this.value = value;
+		this.color = color;
+		this.suit = suit;
+		this.setLetter(null);
+        this.image = image;
+	}
+        
+        public Card (int value, Color color, Suit suit, Letter l, String image)
+	{
+		this.value = value;
+		this.color = color;
+		this.suit = suit;
+		this.setLetter(l);
+        this.image = image;
+	}
+	
+        
+        
+     //***************************************** GETTERS AND SETTERS******************************************
+	/**
+	 * @return the image
+	 */
+	public String getImage() {
+		return image;
+	}
+
+	/**
+	 * @param image the image to set
+	 */
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	/**
+	 * @return value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 *  @param value
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	/**
+	 * @return color
+	 */
+	public Color getColor() {
+		return color;
+	}
+
+	/**
+	 *  @param color
+	 */
+	public void setColor(Color color) {
+		this.color = color;
+	}
+
+	/**
+	 * @return suit
+	 */
+	public Suit getSuit() {
+		return suit;
+	}
+
+	/**
+	 *  @param suit
+	 */
+	public void setSuit(Suit suit) {
+		this.suit = suit;
+	}
+
+	/**
+	 * @return letter
+	 */
+	public Letter getLetter() {
+		return letter;
+	}
+	
+	/**
+	 *  @param letter
+	 */
+	public void setLetter(Letter letter) {
+		this.letter = letter;
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Card [value=" + value + ", color=" + color + ", suit=" + suit
+				+ ", letter=" + letter + "]";
+	}
+	
+	
+	
+
+}
---
+++ Copy (2) of blackJack1/src/model/Hand.java
@@ -0,0 +1,141 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Hand {
+	
+	//***************************************** Variables ******************************************
+	/**KEY field, Hand's serialNumber*/
+	private int serialNum;
+	/**type of hand: dealer/player*/
+	private TypeOfHand TypeOfHand ;
+	/**cards existing in this hand*/
+	private ArrayList<Card> cards;
+	/** amount of cards*/
+	private int amountOfCards;
+	
+	//***************************************** Constructors ******************************************
+  	/**
+  	 * Full C'tor
+  	 */
+	protected Hand(int serialNum, TypeOfHand typeOfHand) {
+		super();
+		this.serialNum = serialNum;
+		TypeOfHand = typeOfHand;
+		this.amountOfCards = 0;
+		this.cards = new ArrayList<Card>();
+
+	}
+	
+	
+	protected Hand(int serialNum,TypeOfHand typeOfHand, int amountOfCards, ArrayList<Card> cards) { 
+		super();
+		this.serialNum = serialNum;
+		TypeOfHand = typeOfHand;
+		this.amountOfCards = amountOfCards;
+		this.cards = cards;
+	}
+
+
+	/**
+	 * @return the serialNum
+	 */
+	protected int getSerialNum() {
+		return serialNum;
+	}
+
+
+	/**
+	 * @param serialNum the serialNum to set
+	 */
+	protected void setSerialNum(int serialNum) {
+		this.serialNum = serialNum;
+	}
+
+
+	/**
+	 * @return the typeOfHand
+	 */
+	protected TypeOfHand getTypeOfHand() {
+		return TypeOfHand;
+	}
+
+
+	/**
+	 * @param typeOfHand the typeOfHand to set
+	 */
+	protected void setTypeOfHand(TypeOfHand typeOfHand) {
+		TypeOfHand = typeOfHand;
+	}
+
+
+	/**
+	 * @return the amountOfCards
+	 */
+	protected int getAmountOfCards() {
+		return amountOfCards;
+	}
+
+
+	/**
+	 * @param amountOfCards the amountOfCards to set
+	 */
+	protected void setAmountOfCards(int amountOfCards) {
+		this.amountOfCards = amountOfCards;
+	}
+	
+	/**
+	 * @return the cards
+	 */
+	public ArrayList<Card> getCards() {
+		return cards;
+	}
+
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Hand [serialNum=" + serialNum + ", TypeOfHand=" + TypeOfHand
+				+ ", cards=" + cards + ", amountOfCards=" + amountOfCards + "]";
+	}
+
+
+	/**
+	 * @param cards the cards to set
+	 */
+	protected void setCards(ArrayList<Card> cards) {
+		this.cards = cards;
+	}
+
+
+	// function which checks if amount of values is legal
+	public boolean isLegal(){
+		if (this.amountOfCards >= 21){
+			return false;
+		}
+		else 
+			return true;
+		
+	}
+	
+	// function which add a card to the dealer/player's hand
+	public boolean addCard(Card c){ 
+		
+		if (isLegal())
+		{
+			
+			this.cards.add(c);
+			this.amountOfCards ++;
+			return true;			
+		}
+		else return false;
+	}
+	
+	
+	
+	
+	
+
+}

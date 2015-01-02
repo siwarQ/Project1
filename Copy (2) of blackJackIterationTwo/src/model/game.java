c89f0f668310a35c8a45c6c0f3503e9a1ee0cafd
siwarQ <socoro92@hotmail.com>
2015-01-02 12:27:17 +0200

    check

--- Copy (2) of blackJackIterationTwo/src/model/game.java
+++ Copy (2) of blackJackIterationTwo/src/model/game.java
@@ -33,6 +33,7 @@ public class game {
     private int amountOfDealerCards = 0;
     
     private boolean status;
+    
 	
   //***************************************** Constructors ******************************************
   	/**
